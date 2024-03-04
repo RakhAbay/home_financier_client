40ec81e479b4e4bc4ce52a84d2ca46ae63dc3049
rakhman <r.abay@gmail.com>
2024-03-04 23:40:42 +0500

    feat: income analytics

--- src/modules/analytics/AnalyticsPage.tsx
+++ src/modules/analytics/AnalyticsPage.tsx
@@ -1,7 +1,24 @@
+import { useQuery } from "react-query";
+import api from "../../common/api";
+import PieChart from "./components/PieChart";
+
 const AnalyticsPage = (): JSX.Element => {
+
+    const outcomeAnalyticsQuery = useQuery(
+        "outcome-analytics",
+        api.analytics.outcomeAnalytics
+    );
+
+    
+    const outcomeAnalytics = outcomeAnalyticsQuery.data?.data
+
+    // console.log('incomeAnalytics: ', incomeAnalytics)
+    console.log('outcomeAnalytics: ', outcomeAnalytics)
+
     return (
         <div>
             <h1>Analytics Page</h1>
+            <PieChart />
         </div>
     )
 }
